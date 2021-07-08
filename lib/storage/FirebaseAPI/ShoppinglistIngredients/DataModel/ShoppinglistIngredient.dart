39bcaec886b53e5b5c22edbd8b7284df670c70e8
Anton <a_widmark@hotmail.com>
2021-07-08 23:04:18 +0200

    added is bought field to datamodel of shoppinglistingredient

--- lib/storage/FirebaseAPI/ShoppinglistIngredients/DataModel/ShoppinglistIngredient.dart
+++ lib/storage/FirebaseAPI/ShoppinglistIngredients/DataModel/ShoppinglistIngredient.dart
@@ -8,9 +8,10 @@ class ShoppinglistIngredient implements DataModel {
 
   double quantity;
   String unit;
+  bool isBought;
 
   ShoppinglistIngredient(
-      {this.reference, this.shoppinglistReference, this.ingredientReference, this.quantity, this.unit});
+      {this.reference, this.shoppinglistReference, this.ingredientReference, this.quantity, this.unit, this.isBought});
 
   ShoppinglistIngredient.fromDocumentSnapshot(DocumentSnapshot ds)
       : this(
@@ -18,15 +19,22 @@ class ShoppinglistIngredient implements DataModel {
             shoppinglistReference: (ds.data() as Map<String, dynamic>)['shoppinglist'] as DocumentReference,
             ingredientReference: (ds.data() as Map<String, dynamic>)['ingredient'] as DocumentReference,
             quantity: (ds.data() as Map<String, dynamic>)['quantity'] as double,
-            unit: (ds.data() as Map<String, dynamic>)['unit'] as String);
+            unit: (ds.data() as Map<String, dynamic>)['unit'] as String,
+            isBought: (ds.data() as Map<String, dynamic>)['is_bought'] as bool);
 
   ShoppinglistIngredient.fromJson(Map<String, Object> json)
       : this(
             shoppinglistReference: json['shoppinglist'] as DocumentReference,
             ingredientReference: json['ingredient'] as DocumentReference,
             quantity: json['quantity'] as double,
-            unit: json['unit'] as String);
+            unit: json['unit'] as String,
+            isBought: json['is_bought'] as bool);
 
-  Map<String, Object> toJson() =>
-      {'shoppinglist': shoppinglistReference, ' ingredient': ingredientReference, 'quantity': quantity, 'unit': unit};
+  Map<String, Object> toJson() => {
+        'shoppinglist': shoppinglistReference,
+        ' ingredient': ingredientReference,
+        'quantity': quantity,
+        'unit': unit,
+        'is_bought': isBought
+      };
 }
--- lib/storage/FirebaseAPI/Shoppinglists/DataModel/Shoppinglist.dart
+++ lib/storage/FirebaseAPI/Shoppinglists/DataModel/Shoppinglist.dart
@@ -5,11 +5,19 @@ class Shoppinglist implements DataModel {
   DocumentReference reference;
   DocumentReference planReference;
 
-  Shoppinglist({this.reference, this.planReference});
+  Timestamp createdAt;
+  Timestamp lastModifiedAt;
+
+  Shoppinglist({this.reference, this.planReference, this.createdAt, this.lastModifiedAt});
   Shoppinglist.fromDocumentSnapshot(DocumentSnapshot ds)
-      : this(reference: ds.reference, planReference: (ds.data() as Map<String, dynamic>)['plan']);
+      : this(
+            reference: ds.reference,
+            planReference: (ds.data() as Map<String, dynamic>)['plan'],
+            createdAt: (ds.data() as Map<String, dynamic>)['created_at'],
+            lastModifiedAt: (ds.data() as Map<String, dynamic>)['last_modified_at']);
 
-  Shoppinglist.fromJson(Map<String, Object> json) : this(planReference: json['plan']);
+  Shoppinglist.fromJson(Map<String, Object> json)
+      : this(planReference: json['plan'], createdAt: json['created_at'], lastModifiedAt: json['last_modified_at']);
 
-  Map<String, Object> toJson() => {'plan': planReference};
+  Map<String, Object> toJson() => {'plan': planReference, 'created_at': createdAt, 'last_modified_at': lastModifiedAt};
 }
