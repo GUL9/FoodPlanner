d9817725b392a88d45feba96b708dea8267d7d80
Anton <a_widmark@hotmail.com>
2021-07-18 01:22:09 +0200

    updated new recipe UI careated recipehelper

--- lib/Views/Recipes/NewRecipeView.dart
+++ lib/Views/Recipes/NewRecipeView.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:grocerylister/Storage/FirebaseAPI/APIs.dart';
-import 'package:grocerylister/Storage/FirebaseAPI/Ingredients/DataModel/Ingredient.dart';
-import 'package:grocerylister/Storage/FirebaseAPI/RecipeIngredients/DataModel/RecipeIngredient.dart';
-import 'package:grocerylister/Storage/FirebaseAPI/Recipes/DataModel/Recipe.dart';
+import 'package:grocerylister/Helpers/RecipeHelper.dart';
+import 'package:grocerylister/Styling/Themes/Themes.dart';
+import 'package:grocerylister/Views/Components/IngredientInputDialog.dart';
 import 'package:grocerylister/util/strings.dart';
-
-import 'package:grocerylister/Views/Components/IngredientInputRow.dart';
 
 class NewRecipeView extends StatefulWidget {
   @override
@@ -15,57 +12,61 @@ class NewRecipeView extends StatefulWidget {
 
 class NewRecipeViewState extends State<NewRecipeView> {
   TextEditingController _recipeNameController = TextEditingController();
-  List<IngredientInputRow> _ingredientRows = [];
-
-  void _addEmptyIngredientInputRow() => setState(() => _ingredientRows.add(IngredientInputRow()));
-
-  void _saveRecipe() async {
-    var recipe = Recipe(name: _recipeNameController.text);
-    recipe.id = await recipesAPI.add(recipe);
-    for (var row in _ingredientRows) {
-      var ingredient = Ingredient(name: row.getName());
-      // TODO: add check if already exist
-      ingredient.id = await ingredientsAPI.add(ingredient);
-      var recipeIngredient = RecipeIngredient(
-          recipeId: recipe.id,
-          ingredientId: ingredient.id,
-          quantity: double.parse(row.getQuantity()),
-          unit: row.getUnit());
-      recipeIngredient.id = await recipeIngredientsAPI.add(recipeIngredient);
-    }
-  }
+  List<Map<String, String>> _ingredientRows = [];
+  ScrollController _ingredientListScrollController = ScrollController();
 
   void _saveRecipeAndReturn() {
-    _saveRecipe();
+    RecipeHelper.saveRecipe(_recipeNameController.text, _ingredientRows);
     Navigator.pop(context);
   }
 
-  Expanded _ingredientList() => Expanded(
+  void _openNewRecipeIngredientDialog() {
+    showDialog(context: context, builder: (_) => IngredientInputDialog()).then((newIngredientData) {
+      if (newIngredientData != null)
+        setState(() {
+          _ingredientRows.add(newIngredientData);
+          _ingredientListScrollController.jumpTo(_ingredientListScrollController.position.maxScrollExtent);
+        });
+    });
+  }
+
+  Widget _newIngredientButton() => Padding(
+      padding: EdgeInsets.all(1),
+      child: Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+              height: 40,
+              width: 500,
+              child: TextButton(
+                  child: Text(Strings.new_ingredient, style: Theme.of(context).textTheme.button),
+                  style: Theme.of(context).textButtonTheme.style,
+                  onPressed: _openNewRecipeIngredientDialog))));
+
+  Widget _ingredientListTile(int index) => ListTile(
+      title: Text(
+          "${_ingredientRows[index]['quantity']} ${_ingredientRows[index]['unit']} ${_ingredientRows[index]['name']} ",
+          style: Theme.of(context).textTheme.bodyText2),
+      trailing: IconButton(icon: Icon(Icons.delete, color: primary3), onPressed: null));
+
+  Widget _ingredientListNEW() => Expanded(
       child: ListView.builder(
-          itemCount: _ingredientRows.length,
-          itemBuilder: (context, index) => Padding(padding: EdgeInsets.only(top: 5), child: _ingredientRows[index])));
+          controller: _ingredientListScrollController,
+          itemCount: _ingredientRows.length + 1,
+          itemBuilder: (context, index) =>
+              Card(child: index == _ingredientRows.length ? _newIngredientButton() : _ingredientListTile(index))));
 
   _isFloatingActionButtonColumnVisible() => MediaQuery.of(context).viewInsets.bottom == 0 ? true : false;
 
-  Visibility _floatingActionButtonColumn() => Visibility(
+  Widget _floatingActionButtonColumn() => Visibility(
       visible: _isFloatingActionButtonColumnVisible(),
       child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
         FloatingActionButton.extended(
             icon: Icon(Icons.mic), label: Text(Strings.add), heroTag: null, onPressed: () => takeVoiceInput()),
         Padding(padding: EdgeInsets.symmetric(vertical: 10)),
         FloatingActionButton.extended(
-            icon: Icon(Icons.add), label: Text(Strings.add), heroTag: null, onPressed: _addEmptyIngredientInputRow),
-        Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-        FloatingActionButton.extended(
             icon: Icon(Icons.check), label: Text(Strings.save_recipe), heroTag: null, onPressed: _saveRecipeAndReturn),
         Padding(padding: EdgeInsets.symmetric(vertical: 10)),
       ]));
-
-  @override
-  void initState() {
-    super.initState();
-    _addEmptyIngredientInputRow();
-  }
 
   @override
   build(BuildContext context) {
@@ -80,7 +81,8 @@ class NewRecipeViewState extends State<NewRecipeView> {
                 TextFormField(controller: _recipeNameController, style: Theme.of(context).textTheme.bodyText2),
                 Padding(padding: EdgeInsets.symmetric(vertical: 10)),
                 Text(Strings.ingredients, style: Theme.of(context).textTheme.headline2),
-                _ingredientList()
+                Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+                _ingredientListNEW()
               ])),
           Align(alignment: Alignment.bottomRight, child: _floatingActionButtonColumn())
         ]));
